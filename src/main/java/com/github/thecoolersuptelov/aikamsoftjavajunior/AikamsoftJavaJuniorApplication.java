41a96d244c2ad7926c9c488167aa2a0a28243ecb
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-06 13:16:55 +0700

    Add build info. Proper building config. Add readme. 	new file:   README.MD 	modified:   pom.xml 	modified:   src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplication.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplication.java
+++ src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplication.java
@@ -1,13 +1,24 @@
 package com.github.thecoolersuptelov.aikamsoftjavajunior;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AikamsoftJavaJuniorApplication {
+public class AikamsoftJavaJuniorApplication implements CommandLineRunner {
+
+	private static Logger logger = LoggerFactory.getLogger(AikamsoftJavaJuniorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AikamsoftJavaJuniorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		for (int i = 0; i < args.length; i++) {
+			logger.info(args[i]);
+		}
+	}
 }
