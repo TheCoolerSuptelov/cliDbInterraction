c490de36d575efb191f50781e29f65d893955641
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-06 15:22:03 +0700

    Changed signature for CliActions method prepareAnswer 	modified:   src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/CliActions.java 	modified:   src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/Search.java 	new file:   src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/Statistic.java

--- src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/CliActions.java
+++ src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/CliActions.java
@@ -3,5 +3,5 @@ package com.github.thecoolersuptelov.aikamsoftjavajunior.Services;
 import com.github.thecoolersuptelov.aikamsoftjavajunior.DTO;
 
 public interface CliActions {
-    public void prepareAnswer();
+    public void prepareAnswer(String inputPath, String outputPath);
 }
--- src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/Statistic.java
+++ src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/Services/Statistic.java
@@ -1,11 +1,8 @@
 package com.github.thecoolersuptelov.aikamsoftjavajunior.Services;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class Search implements CliActions{
+public class Statistic implements CliActions{
     @Override
     public void prepareAnswer(String inputPath, String outputPath) {
-        System.out.println("Search working");
+        System.out.println("stats working");
     }
 }
