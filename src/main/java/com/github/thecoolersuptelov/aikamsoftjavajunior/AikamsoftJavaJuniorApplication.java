c0029a7905bf2ef601191f9bc965395ee34a8a62
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-06 12:04:31 +0700

    Initial commit 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplicationTests.java

---
+++ src/main/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.github.thecoolersuptelov.aikamsoftjavajunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikamsoftJavaJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikamsoftJavaJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/thecoolersuptelov/aikamsoftjavajunior/AikamsoftJavaJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thecoolersuptelov.aikamsoftjavajunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikamsoftJavaJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
